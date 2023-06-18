72df4585c64132f7ba851ddf45a34d4171659731
Kitavtsev Stanislav <Kitavtsev.s@gmail.com>
2023-06-19 00:04:32 +0300

    init commit

---
+++ src/main/java/com/calmcapybara/springinactionreactiveserver/SpringInActionReactiveServerApplication.java
@@ -0,0 +1,13 @@
+package com.calmcapybara.springinactionreactiveserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInActionReactiveServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInActionReactiveServerApplication.class, args);
+    }
+
+}
